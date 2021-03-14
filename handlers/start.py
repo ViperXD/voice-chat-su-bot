c570f113504aa30a086af26d631166782bd7c1c8
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-14 09:42:50 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -11,22 +11,22 @@ async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
 
-I am SU Music Player, an open-source bot that lets you play music in your Telegram groups.
+I am Music Player, an bot that lets you play music in your Telegram groups.
 
 Use the buttons below to know more about me.""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⚒ Source code", url="https://t.me/SuMusicPlayer"
+                        "⚡️My Creator", url="https://t.me/VIvekTVP"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/su_Chats"
+                        "💬 Group", url="https://t.me/VKP_BOTS"
                     ),
                     InlineKeyboardButton(
-                        "Channel 🔈", url="https://t.me/su_Bots"
+                        "Channel 🔈", url="https://t.me/VKPROJECTS"
                     )
                 ]
             ]
